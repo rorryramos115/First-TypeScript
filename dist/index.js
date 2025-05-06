d147b32bc35e109a4e0cc755ddd75fdc5302dc35
rorry <rorryramos115@gmail.com>
2025-05-06 19:00:41 +0800

    install ts and run hello world in static typing

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Static type-checking
+// let myName: string = "Rorry";
+// const message = "Hello World!";
+// console.log(message);
+const announcement = "Hello World!";
+// How quickly can you spot the typos?
+console.log(announcement.toLocaleLowerCase());
+// announcement.toLocalLowerCase();
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+
+// Static type-checking
+// let myName: string = "Rorry";
+const message: string = "Hello World!";
+console.log(message);
