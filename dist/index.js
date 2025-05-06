eebee821c99336be7a94cfa9dc0a92d37fa47e92
rorry <rorryramos115@gmail.com>
2025-05-06 19:25:13 +0800

    give 2 each example of interface & types and Enums

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,34 @@
 "use strict";
-// Static type-checking
-// let myName: string = "Rorry";
-// const message = "Hello World!";
-// console.log(message);
-const announcement = "Hello World!";
-// How quickly can you spot the typos?
-console.log(announcement.toLocaleLowerCase());
-// announcement.toLocalLowerCase();
+// 1. Static type-checking
+//Ex: 1
+const message = "Hello World!";
+console.log("Static type Ex: 1. ", message);
+// Ex: 2
+function greet(name) {
+    return `Hello, ${name}`;
+}
+const Greet = greet("Rorry");
+console.log("Static type Ex: 2. ", Greet);
+const user = { name: "Rorry Ramos", age: 34, noob: true };
+console.log("Interfaces Ex: ", user);
+const point = { x: 10, y: 5 };
+console.log("Types Ex: ", point);
+// =======================================================================
+// 3. Enums
+//  Example 1
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+let color = Color.Green;
+console.log("Enums Ex: 1. ", color);
+//  Example 2
+var Directions;
+(function (Directions) {
+    Directions["Up"] = "Up";
+    Directions["Down"] = "Down";
+})(Directions || (Directions = {}));
+console.log("Enums Ex: 2. ", Directions.Down);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,46 @@
 
-// Static type-checking
-// let myName: string = "Rorry";
+// 1. Static type-checking
+//Ex: 1
 const message: string = "Hello World!";
-console.log(message);
+console.log("Static type Ex: 1. ", message);
+
+// Ex: 2
+function greet(name: string): string {
+    return `Hello, ${name}`
+}
+const Greet: string = greet("Rorry");
+console.log("Static type Ex: 2. ", Greet);
+
+// =======================================================================
+
+// 2. Interfaces & Types
+// Example 1 (Interface):
+interface User {
+    name: string;
+    age: number;
+    noob: Boolean;
+}
+const user: User = { name:"Rorry Ramos", age: 34, noob: true};
+console.log("Interfaces Ex: ", user);
+
+// Example 2 (Type Alias):
+type Point = { x:number, y:number};
+const point: Point = { x: 10, y: 5};
+console.log("Types Ex: ", point);
+
+// =======================================================================
+
+// 3. Enums
+//  Example 1
+enum Color {Red, Green, Blue};
+let color: Color = Color.Green;
+console.log("Enums Ex: 1. ", color);
+
+//  Example 2
+enum Directions {
+    Up = "Up",
+    Down = "Down",
+}
+console.log("Enums Ex: 2. ", Directions.Down);
+
+// ======================================================================
